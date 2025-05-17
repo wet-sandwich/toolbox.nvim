65feb5a12494292faf8555d1700ce3055b4efcad
Andrew Niles <andrewgniles@gmail.com>
2025-05-17 12:07:55 -0600

    add npm tools

---
+++ lua/npm.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+---@return string?
+local function get_current_path()
+	local file_name = vim.fn.expand("%:t")
+	local file_path = vim.fn.expand("%:p:h")
+	if file_name ~= "package.json" then
+		print("Not inside a package.json file")
+		return nil
+	end
+	return file_path
+end
+
+-- Run "npm i" from the current file's directory
+M.npm_i = function()
+	local path = get_current_path()
+	if path == nil then
+		return
+	end
+	vim.cmd("!cd " .. path .. " && npm i")
+end
+
+-- Run "npm i <package>" from the current file's directory
+-- prompts user for package name
+M.npm_i_pkg = function()
+	local path = get_current_path()
+	if path == nil then
+		return
+	end
+	local pkg = vim.fn.input("Package(s): ")
+	vim.cmd("!cd " .. path .. " && npm i " .. pkg)
+end
+
+return M
--- plugin/toolbox.lua
+++ plugin/toolbox.lua
@@ -9,3 +9,11 @@ end, {})
 vim.api.nvim_create_user_command("TBSemverPatch", function()
 	require("semver").increment_semantic_version("patch")
 end, {})
+
+vim.api.nvim_create_user_command("TBNpmInstall", function()
+	require("npm").npm_i()
+end, {})
+
+vim.api.nvim_create_user_command("TBNpmInstallPkg", function()
+	require("npm").npm_i_pkg()
+end, {})

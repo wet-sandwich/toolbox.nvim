9594d5820fffdd361af821f539849e236feda25e
Andrew Niles <andrewgniles@gmail.com>
2025-05-16 21:45:14 -0600

    rename semver commands

--- plugin/toolbox.lua
+++ plugin/toolbox.lua
@@ -1,11 +1,11 @@
-vim.api.nvim_create_user_command("TbMajor", function()
+vim.api.nvim_create_user_command("TBSemverMajor", function()
 	require("semver").increment_semantic_version("major")
 end, {})
 
-vim.api.nvim_create_user_command("TbMinor", function()
+vim.api.nvim_create_user_command("TBSemverMinor", function()
 	require("semver").increment_semantic_version("minor")
 end, {})
 
-vim.api.nvim_create_user_command("TbPatch", function()
+vim.api.nvim_create_user_command("TBSemverPatch", function()
 	require("semver").increment_semantic_version("patch")
 end, {})

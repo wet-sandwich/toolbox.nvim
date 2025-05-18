371874153714cd85d735aefb0b7a5f573666a33c
Andrew Niles <andrewgniles@gmail.com>
2025-05-17 22:47:49 -0600

    refactor npm tool into a single command

--- lua/npm.lua
+++ lua/npm.lua
@@ -11,24 +11,27 @@ local function get_current_path()
 	return file_path
 end
 
--- Run "npm i" from the current file's directory
-M.npm_i = function()
-	local path = get_current_path()
-	if path == nil then
-		return
-	end
-	vim.cmd("!cd " .. path .. " && npm i")
-end
+---@enum
+local modes = {
+	all = 1,
+	package = 2,
+}
 
--- Run "npm i <package>" from the current file's directory
--- prompts user for package name
-M.npm_i_pkg = function()
+-- Run "npm i" from the current file's directory, has two modes, all and package
+---@param mode string?
+M.npm_i = function(mode)
+	mode = mode == "" and "all" or mode
+	assert(modes[mode] ~= nil, "Invalid mode selected, valid options [all|package]")
 	local path = get_current_path()
 	if path == nil then
 		return
 	end
-	local pkg = vim.fn.input("Package(s): ")
-	vim.cmd("!cd " .. path .. " && npm i " .. pkg)
+	local pkg_list = ""
+	if modes[mode] == modes.package then
+		pkg_list = vim.fn.input("Package(s): ")
+	end
+	local cmd = string.format("!cd %s && npm i %s", path, pkg_list)
+	vim.cmd(cmd)
 end
 
 return M
--- plugin/toolbox.lua
+++ plugin/toolbox.lua
@@ -2,15 +2,10 @@ vim.api.nvim_create_user_command("TBIncSemver", function(args)
 	require("semver").increment_semantic_version(args.args)
 end, { nargs = "?" })
 
-vim.api.nvim_create_user_command("TBNpmInstall", function()
-	require("npm").npm_i()
-end, {})
-
-vim.api.nvim_create_user_command("TBNpmInstallPkg", function()
-	require("npm").npm_i_pkg()
-end, {})
+vim.api.nvim_create_user_command("TBNpmInstall", function(args)
+	require("npm").npm_i(args.args)
+end, { nargs = "?" })
 
 vim.api.nvim_create_user_command("TBLogVariable", function(args)
-	print(vim.inspect(args))
 	require("logger").log_variable(args.args)
 end, { nargs = "?" })

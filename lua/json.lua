47b7ea2a3ae302ac5ae7e725e3eaae4e4dc44e82
Andrew Niles <andrewgniles@gmail.com>
2025-05-24 15:03:47 -0600

    add json parser

--- lua/json.lua
+++ lua/json.lua
@@ -1,11 +1,6 @@
 local M = {}
 
 local format = function()
-	if vim.fn.executable("jq") ~= 1 then
-		vim.notify("Must have jq installed to format json", vim.log.levels.ERROR)
-		return
-	end
-
 	local buf_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 	local result = vim.system({ "jq", "." }, { stdin = buf_lines }):wait()
 
@@ -18,9 +13,31 @@ local format = function()
 	vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
 end
 
+local parse = function()
+	local line = vim.api.nvim_get_current_line()
+	if line == "" then
+		return
+	end
+	local result = vim.system({ "jq", "-r" }, { stdin = line }):wait()
+	if result.code ~= 0 then
+		vim.notify("Error parsing JSON: " .. result.stderr, vim.log.levels.ERROR)
+		return
+	end
+	local lines = vim.fn.split(result.stdout, "\n", false)
+	local row = vim.api.nvim_win_get_cursor(0)[1]
+	vim.api.nvim_buf_set_lines(0, row - 1, row, false, lines)
+end
+
 M.json = function(mode)
+	if vim.fn.executable("jq") ~= 1 then
+		vim.notify(string.format("Must have jq installed to %s JSON", mode), vim.log.levels.ERROR)
+		return
+	end
+
 	if mode == "format" then
 		format()
+	elseif mode == "parse" then
+		parse()
 	else
 		vim.notify("Invalid mode " .. mode, vim.log.levels.ERROR)
 	end
